e5a0573803f4c8c28a44591169ed0fe7a9cec9f3
Jesus <je.flores28@gmail.com>
2018-02-16 19:23:05 -0600

    Committing

---
+++ src/models/users.model.js
@@ -0,0 +1,43 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require("sequelize");
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function(app) {
+  const sequelizeClient = app.get("sequelizeClient");
+  const users = sequelizeClient.define(
+    "users",
+    {
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      }
+    },
+    {
+      hooks: {
+        beforeCount(options) {
+          options.raw = true;
+        }
+      }
+    }
+  );
+
+  users.associate = function(models) {
+    // eslint-disable-line no-unused-vars
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+    users.hasMany(models.auctions, { foreignKey: "seller_id" });
+  };
+
+  return users;
+};
